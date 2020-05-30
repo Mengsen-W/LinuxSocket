9c9f94adbeb452c288b1266a8ecf6f2ac81e05db
Mengsen-W <mengsen_wang@163.com>
2020-05-30 17:34:45 +0800

    coroutine

--- lua/hello.lua
+++ lua/hello.lua
@@ -20,3 +20,21 @@ print(type(true))               --> boolean
 print(type(nil))                --> nil
 print(type(type(X)))            --> string
 
+-- 使用两个方括号来表示一块字符
+html = [[
+<html>
+<head></head>
+<body>
+    <a href="http://https::///">test</a>
+    </body>
+    </html>
+]]
+print(html)
+
+print("2".."8")
+print('2'+'8')
+print('2'+8)
+-- must blank with '..' whit number
+print(125 .. 24)
+
+print(#html)
