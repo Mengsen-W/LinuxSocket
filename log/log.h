a4a9e13f2a8a0dca1c67b3864ee018749f5d9bd9
Mengsen-W <mengsen_wang@163.com>
2020-06-05 17:59:57 +0800

    update log

--- log/log.h
+++ log/log.h
@@ -6,15 +6,20 @@
  */
 
 #include <cstdint>
-#include <memory>  // for unique_ptr
-#include <string>  // for size_t
+#include <iostream>  // for ostream
+#include <memory>    // for unique_ptr
+#include <string>    // for size_t
 
 #ifndef __MENGSEN_LOG_H__
 #define __MENGSEN_LOG_H__
+
 namespace mengsen_log {
 
 /**
- * enum class of log level
+ * @brief: enum class of log level
+ * @enum:
+ * DEBUG,INFO,NOTICE,WARNING,
+ * ERROR,CRIT,ALERT,EMERGE,FATAL
  */
 enum class LogLevel : uint8_t {
   DEBUG,
@@ -28,13 +33,68 @@ enum class LogLevel : uint8_t {
   FATAL
 };
 
+/**
+ * @brief: to set log line
+ */
 class LogLine {
  public:
   LogLine(LogLevel level, const char *filename, const char *function,
           uint32_t line);
   ~LogLine();
+  LogLine(LogLine &&) = default;
+  LogLine &operator=(LogLine &) = default;
+
+  void stringify(std::ostream &os);
+
+  LogLine &operator<<(char arg);
+  LogLine &operator<<(const std::string &arg);
+  LogLine &operator<<(uint32_t arg);
+  LogLine &operator<<(int32_t arg);
+  LogLine &operator<<(uint64_t arg);
+  LogLine &operator<<(int64_t arg);
+  LogLine &operator<<(double arg);
+
+  template <size_t N>
+  LogLine &operator<<(const char (&arg)[]) {
+    encode(string_literal_t(arg));
+  }
+
+  // if arg same as const char* or char *return LogLine
+  template <typename Arg>
+  typename std::enable_if<std::is_same<Arg, const char *>::value,
+                          LogLine &>::type
+  operator<<(const Arg &arg) {
+    encode(arg);
+    return *this;
+  }
+
+  template <typename Arg>
+  typename std::enable_if<std::is_same<Arg, char *>::value, LogLine &>::type
+  operator<<(const Arg &arg) {
+    encode(arg);
+    return *this;
+  }
+
+  struct string_literal_t {
+    explicit string_literal_t(const char *s) : _s(s) {}
+    const char *_s;
+  };
 
  private:
+  char *buffer();
+
+  template <typename Arg>
+  void encode(Arg arg);
+  template <typename Arg>
+  void encode(Arg arg, uint8_t type_id);
+  void encode(char *arg);
+  void encode(const char *arg);
+  void encode(string_literal_t arg);
+  void encode_c_string(const char *arg, size_t length);
+
+  void resize_buffer_if_needed(size_t additional_bytes);
+
+  void stringify(std::ostream &os, char *start, const char *const end);
   size_t _bytes_used;
   size_t _buffer_size;
   std::unique_ptr<char[]> _heap_buffer;
@@ -42,5 +102,112 @@ class LogLine {
       256 - 2 * sizeof(size_t) - sizeof(decltype(_buffer_size)) - 8];
 };
 
+//? non-clear the class used
+struct Log {
+  bool operator==(LogLine &);
+};
+
+/**
+ * @brief: set log level
+ * @param: LogLevel level
+ * @return: void
+ */
+void set_log_level(LogLevel level);
+
+/**
+ * @brief: Determines whether the log level is higher than the set log level
+ * @param: LogLevel level
+ * @return: if higher than set return true else return false
+ */
+bool is_logged(LogLevel level);
+
+/**
+ * Non guaranteed Log
+ * Using a ring buffer to hold log lines
+ * when the ring buffer is full, the previous log will be dropped
+ * doesn't block producer even if the ring buffer is full
+ * LogLines are 256 bytes
+ * ring buffer size = ring_buffer_size_mb* 1024* 1024/ 256
+ */
+struct NonGuaranteedLogger {
+  NonGuaranteedLogger(uint32_t ring_buffer_size_mb)
+      : _ring_buffer_size_mb(ring_buffer_size_mb) {}
+
+  uint32_t _ring_buffer_size_mb;
+};
+
+// TODO undefine
+/**
+ * guaranteed log
+ */
+struct GuaranteedLogger {};
+
+/**
+ * @brief:ensure initialize() is called proior to any log
+ * @param:
+ *  NonGuranteedLogger gl, [used to set ring buffer]
+ *  const std::string &log_directorary, [where to creat logs]
+ *  const std::string &log_file_name, [log file name]
+ *  uint32_t log_file_roll_size_mb [mega bytes after roll next log file]
+ * @return: void
+ */
+void initialize(NonGuaranteedLogger ngl, const std::string &log_directorary,
+                const std::string &log_file_name,
+                uint32_t log_file_roll_size_mb);
+
+/**
+ * @brief: Guaranteed version of initialize
+ */
+void initialize(GuaranteedLogger gl, const std::string &log_directorary,
+                const std::string &log_file_name,
+                uint32_t log_file_roll_size_mb);
+
 }  // namespace mengsen_log
+
+/**
+ * @brief: warp init Log class and LogLine class
+ * @param: LEVEL [LogLevel]
+ * @return: bool
+ */
+#define LOG(LEVEL)      \
+  mengsen_log::Log() == \
+      mengsen_log::LogLine(LEVEL, __FILE__, __func__, __LINE__)
+
+// log debug
+#define LOG_DEBUG                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::DEBUG) && \
+      LOG(mengsen_log::LogLevel::DEBUG)
+// log info
+#define LOG_INFO                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::INFO) && \
+      LOG(mengsen_log::LogLevel::INFO)
+// log notices
+#define LOG_NOTICE                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::NOTICE) && \
+      LOG(mengsen_log::LogLevel::NOTICE)
+// log warring
+#define LOG_WARRING                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::WARNING) && \
+      LOG(mengsen_log::LogLevel::WARNING)
+// log error
+#define LOG_ERROR                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::ERROR) && \
+      LOG(mengsen_log::LogLevel::ERROR)
+// log crit
+#define LOG_CRIT                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::CRIT) && \
+      LOG(mengsen_log::LogLevel::CRIT)
+// log alert
+#define LOG_ALERT                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::ALERT) && \
+      LOG(mengsen_log::LogLevel::ALERT)
+// log emerge
+#define LOG_EMERGE                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::EMERGE) && \
+      LOG(mengsen_log::LogLevel::EMERGE)
+// log fatal
+#define LOG_FATAL                                         \
+  mengsen_log::is_logger(mengsen_log::LogLevel::FATAL) && \
+      LOG(mengsen_log::LogLevel::FATAL)
+
 #endif  // __MENGSEN_LOG_H__
