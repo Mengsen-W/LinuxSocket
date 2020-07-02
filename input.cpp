96332309002ba103da9f1c8b31036ea574e3971b
Mengsen-W <mengsen_wang@163.com>
2020-07-02 22:43:43 +0800

    input

--- input.cpp
+++ input.cpp
@@ -1,10 +1,13 @@
 #include <algorithm>
 #include <iostream>
+#include <regex>
 #include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
+
+bool boolToString(bool input) { return input ? "True" : "False"; }
 
 int stringToInteger(string input) { return stoi(input); }
 
@@ -35,12 +38,43 @@ vector<int> stringToIntegerVector(string input) {
   return output;
 }
 
+string integerVectorToString(vector<int> list, int length = -1) {
+  if (length == -1) {
+    length = list.size();
+  }
+
+  if (length == 0) {
+    return "[]";
+  }
+
+  string result;
+  for (int index = 0; index < length; index++) {
+    int number = list[index];
+    result += to_string(number) + ", ";
+  }
+  return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+void stringTo2DIntegerVector() {
+  std::string text = "[1,2],[1,2]";
+  std::regex ws_re("/[./],");  // whitespace
+  std::vector<std::string> v(
+      std::sregex_token_iterator(text.begin(), text.end(), ws_re, -1),
+      std::sregex_token_iterator());
+  for (auto &&s : v) std::cout << s << "\n";
+}
+
 int main() {
+  stringTo2DIntegerVector();
   string line;
   while (getline(cin, line)) {
-    int s = stringToInteger(line);
-    getline(cin, line);
+    // int s = stringToInteger(line);
+    // std::cout << "Integer = " << s << std::endl;
+    // getline(cin, line);
     vector<int> nums = stringToIntegerVector(line);
+    for (int num : nums) {
+      std::cout << num << std::endl;
+    }
 
     // int ret = Solution().minSubArrayLen(s, nums);
 
