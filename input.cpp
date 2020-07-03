9247856353d8f9f9f1c3f7e0801f25420d9667f7
Mengsen-W <mengsen_wang@163.com>
2020-07-03 21:29:40 +0800

    input

--- input.cpp
+++ input.cpp
@@ -1,11 +1,28 @@
+/*
+ * @Author: Mengsen.Wang
+ * @Date: 2020-07-03 21:28:50
+ * @Last Modified by:   Mengsen.Wang
+ * @Last Modified time: 2020-07-03 21:28:50
+ */
+
+#include <json/json.h>
+
 #include <algorithm>
 #include <iostream>
+#include <queue>
 #include <regex>
 #include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
 bool boolToString(bool input) { return input ? "True" : "False"; }
 
@@ -56,21 +73,93 @@ string integerVectorToString(vector<int> list, int length = -1) {
 }
 
 void stringTo2DIntegerVector() {
-  std::string text = "[1,2],[1,2]";
-  std::regex ws_re("/[./],");  // whitespace
-  std::vector<std::string> v(
-      std::sregex_token_iterator(text.begin(), text.end(), ws_re, -1),
-      std::sregex_token_iterator());
-  for (auto &&s : v) std::cout << s << "\n";
+  string strJsonContent =
+      "{\"list\" : [{ \"camp\" : \"alliance\",\"occupation\" : \"paladin\",\"role_id\" : 1}, \
+        {\"camp\" : \"alliance\",\"occupation\" : \"Mage\",\"role_id\" : 2}],\"type\" : \"roles_msg\",\"valid\" : true}";
+  string strJsontest = "[[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]]";
+  string strType;
+  int nRoleDd = 0;
+  string strOccupation;
+  string strCamp;
+
+  Json::Reader reader;
+  Json::Value root;
+
+  vector<vector<int>> output;
+
+  if (reader.parse(strJsontest, root)) {
+    int size_y = root.size();
+    int size_x = root[0].size();
+
+    for (int i = 0; i < size_y; i++) {
+      vector<int> temp;
+      for (int j = 0; j < size_x; j++) {
+        temp.push_back(root[i][j].asInt());
+      }
+      output.push_back(temp);
+    }
+  }
+
+  for (auto &vec : output) {
+    for (int num : vec) {
+      std::cout << num;
+    }
+    std::cout << std::endl;
+  }
+
+  // // 用reader解析str，并存入root
+  // if (reader.parse(strJsonContent, root)) {
+  //   // 获取非数组内容
+  //   strType = root["type"].asString();
+  //   cout << "type is: " << strType << endl;
+
+  //   // 获取数组内容
+  //   if (root["list"].isArray()) {
+  //     int nArraySize = root["list"].size();
+  //     for (int i = 0; i < nArraySize; i++) {
+  //       nRoleDd = root["list"][i]["role_id"].asInt();
+  //       strOccupation = root["list"][i]["occupation"].asString();
+  //       strCamp = root["list"][i]["camp"].asString();
+
+  //       cout << "role_id is: " << nRoleDd << endl;
+  //       cout << "occupation is: " << strOccupation << endl;
+  //       cout << "camp is: " << strCamp << endl;
+  //     }
+  //   }
+  // }
+  return;
+}
+
+string treeNodeToString(TreeNode *root) {
+  if (root == nullptr) {
+    return "[]";
+  }
+
+  string output = "";
+  queue<TreeNode *> q;
+  q.push(root);
+  while (!q.empty()) {
+    TreeNode *node = q.front();
+    q.pop();
+
+    if (node == nullptr) {
+      output += "null, ";
+      continue;
+    }
+
+    output += to_string(node->val) + ", ";
+    q.push(node->left);
+    q.push(node->right);
+  }
+  return "[" + output.substr(0, output.length() - 2) + "]";
 }
 
 int main() {
-  stringTo2DIntegerVector();
   string line;
   while (getline(cin, line)) {
-    // int s = stringToInteger(line);
-    // std::cout << "Integer = " << s << std::endl;
-    // getline(cin, line);
+    int s = stringToInteger(line);
+    std::cout << "Integer = " << s << std::endl;
+    getline(cin, line);
     vector<int> nums = stringToIntegerVector(line);
     for (int num : nums) {
       std::cout << num << std::endl;
